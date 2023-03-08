cac40e121c281a6b6ad1ae5ec645a1fda5c5f402
Venomorous <94299601+Venomorous@users.noreply.github.com>
2023-03-08 20:21:39 +0200

    Added Task 2

---
+++ OOPLab6T/Task2.cpp
@@ -0,0 +1,63 @@
+// Create an abstract function class with a virtual function to calculate the value with the x coordinate. Create derived classes: line, ellipse, hyperbola with their functions for calculating the value by the x coordinate. Equation of a straight line: y=ax+b, ellipse, hyperbola
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Function
+{
+protected:
+    double a, b;
+
+public:
+    Function(double a, double b) : a(a), b(b) {}
+    virtual double calculate(double x) = 0;
+};
+
+class Line : public Function
+{
+public:
+    Line(double a, double b) : Function(a, b) {}
+    double calculate(double x)
+    {
+        cout << "Line: " << endl;
+        return a * x + b;
+    }
+};
+
+class Ellipse : public Function
+{
+public:
+    Ellipse(double a, double b) : Function(a, b) {}
+    double calculate(double x)
+    {
+        // return sqrt(a * a - x * x) + b;
+        // return pow((x / a), 2) + pow((y / b), 2);
+        cout << "Ellipse: " << endl;
+        return b * sqrt(1 - (x * x) / (a * a));
+    }
+};
+
+class Hyperbola : public Function
+{
+public:
+    Hyperbola(double a, double b) : Function(a, b) {}
+    double calculate(double x)
+    {
+        cout << "Hyperbola: " << endl;
+        // return sqrt(a * a + x * x) + b;
+        return b * sqrt((x * x) / (a * a) - 1);
+    }
+};
+
+int main()
+{
+    Function *f1 = new Line(1, 2);
+    Function *f2 = new Ellipse(3, 2);
+    Function *f3 = new Hyperbola(1, 2);
+    cout << f1->calculate(1) << endl;
+    cout << f2->calculate(2) << endl;
+    cout << f3->calculate(2) << endl;
+    return 0;
+}
