754e197c549121d1a0a5e67c8ee5bf51b66d0e17
Venomorous <94299601+Venomorous@users.noreply.github.com>
2023-03-08 21:13:36 +0200

    Created some examples for Task 3

--- OOPLab6T/Task3.cpp
+++ OOPLab6T/Task3.cpp
@@ -16,31 +16,43 @@ public:
 
 class Father : public virtual Worker
 {
+protected:
+    int age;
+    string childName;
+
 public:
-    Father(string name, int salary) : Worker(name, salary) {}
+    Father(string name, int salary, int age, string childName) : Worker(name, salary), age(age), childName(childName) {}
     void print()
     {
-        cout << "Father: " << name << " " << salary << endl;
+        cout << "Father: "
+                "\nName: "
+             << name << "\nSalary: " << salary << "\nAge: " << age << "\nName of a child: " << childName << endl;
     }
     virtual ~Father() {}
 };
 
 class FatherWorker : public Father, public virtual Worker
 {
+protected:
+    int childAge;
+
 public:
-    FatherWorker(string name, int salary) : Worker(name, salary), Father(name, salary) {}
+    FatherWorker(string name, int salary, int age, string childName, int childAge) : Worker(name, salary), Father(name, salary, age, childName), childAge(childAge) {}
     virtual ~FatherWorker() {}
     void print()
     {
-        cout << "FatherWorker: " << name << " " << salary << endl;
+        cout << "FatherWorker: "
+             << "\nName: "
+             << name << "\nSalary: " << salary << "\nAge: " << age << "\nName of a child: " << childName << "\nAge of a child: " << childAge << endl;
     }
 };
 
 int main()
 {
-    Worker *w1 = new Father("Ivan", 1000);
-    Worker *w2 = new FatherWorker("Petro", 2000);
+    Worker *w1 = new Father("Ivan", 1000, 20, "Dmytro");
+    Worker *w2 = new FatherWorker("Petro", 2000, 30, "Valentina", 5);
     w1->print();
+    cout << endl;
     w2->print();
     return 0;
 }
